e7b72f7c729e97d15a8de327a37c0e1a63028d98
Durga-velu <durgadevi134babu@gmail.com>
2024-12-13 10:47:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function isSameType(value1, value2) {
   //your js code here
-if( typeof value1===typeof value2){ if(((typeof value1===NaN) !== (typeof value2===NaN))){
+if( typeof value1===typeof value2){ 
+	if(((typeof value1===NaN) && (typeof value2!==NaN)) || ((typeof value2===NaN) && (typeof value1!==NaN))){
         return false;
     }
 								   else{
