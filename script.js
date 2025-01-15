98d84586d2e87d7db63252f3b022505e2533478a
Durga-velu <durgadevi134babu@gmail.com>
2025-01-15 18:43:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function isSameType(value1, value2) {
   //your js code here
 if( typeof value1===typeof value2){ 
-	 if(isNaN(value1) && Number(value2)){
+	 if(isNaN(value1) && Number(value2) ){
         return false;
-
+		 
     }
   	return true;
 } 
